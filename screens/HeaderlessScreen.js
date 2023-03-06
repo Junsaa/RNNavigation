a7ac4fa937167a475d23d058749fa02d86dfb7df
Junsaa <limjs95@gmail.com>
2023-03-06 12:29:29 +0900

    ReactNativeNavigation- naviation,push,pop..

---
+++ screens/HeaderlessScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, StyleSheet,Text,Button} from 'react-native';
+import {SafeAreaView} from "react-native-safe-area-context";
+
+function HeaderlessScreen({navigation}){
+    return(
+      <SafeAreaView>
+        <View>
+          <Text>Header가 없네?</Text>
+          <Button title={"뒤로가기"} onPress={()=>navigation.pop()}/>
+        </View>
+      </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default HeaderlessScreen
