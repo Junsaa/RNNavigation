47d14297499fe8dbe382f63b23274e962c3f7914
Junsaa <limjs95@gmail.com>
2023-03-07 13:38:33 +0900

    ReactNativeNavigation- Materaial Top Tab Navigator

--- App.js
+++ App.js
@@ -1,48 +1,22 @@
 import React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
-import { View, Text, TouchableOpacity, Button } from "react-native";
-import {createDrawerNavigator} from "@react-navigation/drawer";
-import {SafeAreaView} from "react-native-safe-area-context";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MainScreen from "./screens/MainScreen";
+import DetailScreen from "./screens/DetailScreen";
 
-const Drawer = createDrawerNavigator();
+const Stack = createNativeStackNavigator();
 
-function HomeScreen({navigation}){
-  return(
-    <View>
-      <Text>Home</Text>
-      <Button title={"Drawer 열기"} onPress={()=>navigation.openDrawer()}/>
-      <Button title={"Setting 열기"} onPress={()=>navigation.navigate('Setting')} />
-    </View>
-  )
-}
-function SettingScreen({navigation}){
-  return(
-    <View>
-      <Text>Setting</Text>
-      <Button title={"뒤로가기"} onPress={()=>navigation.goBack()} />
-    </View>
-  )
-}
 
 function App(){
     return(
       <NavigationContainer>
-        <Drawer.Navigator
-          initialRouteName={"Home"} drawerPosition={"left"} backBehavior={"history"}
-          drawerContent={({ navigation })=>(
-            <SafeAreaView>
-              <Text>A Custom Drawer</Text>
-              <Button title={"Draw 닫기"} onPress={()=> navigation.closeDrawer()} />
-            </SafeAreaView>
-          )}
-        >
-          <Drawer.Screen
-            name={"Home"}
-            component={HomeScreen}
-            options={{title:'홈',headerLeft:()=> <Text>Left</Text>}}
+        <Stack.Navigator>
+          <Stack.Screen
+            name={"Main"}
+            component={MainScreen}
           />
-          <Drawer.Screen name={"Setting"} component={SettingScreen} options={{title:'설정'}}/>
-        </Drawer.Navigator>
+          <Stack.Screen name={'Detail'} component={DetailScreen} />
+        </Stack.Navigator>
       </NavigationContainer>
     )
 }
